b8b54e6b10cb363d2a0f6758374006dd6484ba01
Andrey Kolybelnikov <a.kolybelnikov@gmail.com>
2022-04-17 02:06:54 +0200

    Encrypt and decrypt message

--- etl/etl.go
+++ etl/etl.go
@@ -1,13 +1,22 @@
 package etl
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/json"
 	"github.com/akolybelnikov/go_serialized/utils"
+	"io"
 	"io/ioutil"
 )
 
 type Message struct {
 	Body string `json:"body"`
+}
+
+type EncryptedMessage struct {
+	Passphrase []byte `json:"passphrase"`
+	Message    []byte `json:"message"`
 }
 
 func ReadData(filename string) (message Message) {
@@ -18,4 +27,43 @@ func ReadData(filename string) (message Message) {
 	utils.LogErrors(err)
 
 	return message
+}
+
+func (m Message) EncodeData(salt []byte) EncryptedMessage {
+	text, key := []byte(m.Body), salt
+
+	c, err := aes.NewCipher(key)
+	utils.LogErrors(err)
+
+	gcm, err := cipher.NewGCM(c)
+	utils.LogErrors(err)
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	_, err = io.ReadFull(rand.Reader, nonce)
+	utils.LogErrors(err)
+
+	return EncryptedMessage{
+		Passphrase: key,
+		Message:    gcm.Seal(nonce, nonce, text, nil),
+	}
+}
+
+func (m EncryptedMessage) DecodeData() string {
+	key := m.Passphrase
+	cipherText := m.Message
+
+	c, err := aes.NewCipher(key)
+	utils.LogErrors(err)
+
+	gcm, err := cipher.NewGCM(c)
+	utils.LogErrors(err)
+
+	nonceSize := gcm.NonceSize()
+
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, cipherText, nil)
+	utils.LogErrors(err)
+
+	return string(plaintext)
 }
--- etl/etl_test.go
+++ etl/etl_test.go
@@ -1,6 +1,7 @@
 package etl
 
 import (
+	"github.com/akolybelnikov/go_serialized/utils"
 	"path/filepath"
 	"testing"
 )
@@ -11,5 +12,26 @@ func TestLoadMessageFromJson(t *testing.T) {
 	msg := ReadData(filename)
 	if msg.Body != want.Body {
 		t.Fatalf(`{Received %s, wanted %s}`, msg.Body, want.Body)
+	}
+}
+
+func TestEncryptMessage(t *testing.T) {
+	filename := filepath.Join("../inputs", "message_one.json")
+	msg := ReadData(filename)
+	passphrase := utils.MakePassphrase()
+	e := msg.EncodeData(passphrase)
+	if len(e.Message) == 0 || len(e.Passphrase) == 0 {
+		t.Fatalf("encrypted message has empty fields %v", e)
+	}
+}
+
+func TestDecryptMessage(t *testing.T) {
+	filename := filepath.Join("../inputs", "message_one.json")
+	msg := ReadData(filename)
+	passphrase := utils.MakePassphrase()
+	e := msg.EncodeData(passphrase)
+	de := e.DecodeData()
+	if msg.Body != de {
+		t.Fatalf(`{Received %s, wanted %s}`, de, msg.Body)
 	}
 }
